94c27a1c470f95adb9e0d41a319e5b308b9a4b6c
MINH TRI <tritornado1234@gmai.com>
2022-07-18 21:13:15 +0700

    fixing ui task dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2 } from '@angular/core';
+import {  transferArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList_test';
+  todo = [{
+    title: 'my daily life  my daily life hihimy daily life hihi my daily life hihi', content:'remember to do home work'
+  }];
+  inprogress = [{
+    title: 'sleep', content:'sleep for 8 hours'}];
+  done = [{
+    title: 'success', content:'yay!! I am done'
+  }];
+  drop(event: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+  panelOpenState = false;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
